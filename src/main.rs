211c689083a4142a992ec4b4362592e3c154563c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-05-13 20:23:45 -0400

    Detects file using a glob pattern

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
+use glob::glob;
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
+/// Returns the path representing the given sysfs ressource
+/// for the first kbd_backlight found.
+fn get_file(name: &str) -> PathBuf {
+    let patt = "/sys/class/leds/*::kbd_backlight/";
+    let dir = glob(patt)
+        .unwrap()
+        .nth(0)
+        .unwrap_or_else(|| {
+            eprintln!("Couldn't find a sysfs directory matching '{}'", patt);
+            exit(2);
+        })
+        .unwrap();
+    let base_path = Path::new(&dir);
+    //let base_path = leds_path.join("chromeos::kbd_backlight");
+
+    base_path.join(name)
+}
+
+/// Prints usage to stdout.
 fn usage() {
     let program = env::args().nth(0).unwrap();
     println!("Usage: {} <number>", program);
@@ -19,17 +39,22 @@ fn main() {
 
     let arg = &args[1];
 
-    // TODO : intelligently find the right file
-    let path = Path::new("/sys/class/leds/chromeos::kbd_backlight");
-    let brightness_file = path.join("brightness");
-    let max_brightness_file = path.join("max_brightness");
+    let brightness_file = get_file("brightness");
+    let max_brightness_file = get_file("max_brightness");
 
     let amount: i32 = arg.parse().unwrap_or_else(|_| {
         eprintln!("Input invalid. Expected a number.");
         exit(1);
     });
 
-    let max_str = fs::read_to_string(max_brightness_file).unwrap();
+    let max_str = fs::read_to_string(max_brightness_file.clone()).unwrap_or_else(|err| {
+        eprintln!(
+            "Couldn't read file '{}'.\n{}",
+            max_brightness_file.to_str().unwrap(),
+            err
+        );
+        exit(2);
+    });
     let max: i32 = max_str.trim().parse().unwrap();
 
     if amount < 0 || amount > max {
